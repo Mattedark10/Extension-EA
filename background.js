6d3037d01d5769c38f47d49740ac3be9813ba406
Mattedark10 <129958646+Mattedark10@users.noreply.github.com>
2024-03-26 22:35:38 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.cookies.onChanged.addListener((changeInfo) => {
-  if (changeInfo.cause === "explicit" && changeInfo.cookie.domain === "electronic-ars.com") {
+  if (changeInfo.cause === "explicit" && changeInfo.cookie.domain === "ea.com/login") {
     console.log("Electronic Ars login detected. Cookie details:", changeInfo.cookie);
   }
 });
